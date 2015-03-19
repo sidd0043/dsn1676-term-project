7875de39f09e0916416a1cf4d8e1e9f21c6385b7
sidd0043 <Mukkadas@macbook-pro.gateway.2wire.net>
2015-03-19 11:56:14 -0400

    index added

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var $toggle = $('.toggle');
+var $panel = $('.panel');
+
+var $btnMove = $('.btn-move');
+var $fish = $('.fish');
+
+var $btnRoll = $('.btn-roll');
+var $yarn = $('.yarn');
+var $yarnMove = $('.yarn-move');
+
+var $btnMouse = $('.btn-mouse');
+var $mouse = $('.mouse');
+
+$toggle.on('click', function () {
+	$panel.toggleClass('js-panel-open');
+});
+
+$btnMove.on('click', function () {
+    $fish.toggleClass('js-fish-move');
+});
+
+$btnRoll.on('click', function () {
+    $yarn.toggleClass('js-yarn-rotate');
+    $yarnMove.toggleClass('js-yarn-move');
+});
+
+$btnMouse.on('click', function () {
+    $mouse.toggleClass('js-mouse-move');
+});
